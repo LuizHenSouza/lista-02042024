d6d5da30d1250dc2a13268eeed8ae552e1112acc
Luiz Souza <LuizCraftPB@Gmail.com>
2024-04-02 20:30:23 -0300

    fix_correcao_nome_funcoes

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -1,30 +1,30 @@
-def cumprimentar_usuario():
+def cumprimenta_usuario():
     nomeUsuario = str(input("Digite seu nome:"))
     print(f"Olá, {nomeUsuario}! Seja bem-vindo(a)!")
 
-cumprimentar_usuario()
+cumprimenta_usuario()
 
-def somar_numeros():
+def soma_numeros():
     numero1 = float(input("Digite um número: "))
     numero2 = float(input("Digite outro número: "))
     print(f"A soma dos números é: {(numero1+numero2)}")
 
-somar_numeros()
+soma_numeros()
 
 
-def multiplicar_numeros():
+def multiplica_numeros():
     numero1 = float(input("Digite um número: "))
     numero2 = float(input("Digite outro número: "))
     print(f"O produto dos números é: {(numero1*numero2)}")
 
-multiplicar_numeros()
+multiplica_numeros()
 
 
-def divisao_por_dois():
+def divide_por_dois():
     numero1 = float(input("Digite um número: "))
     print(f"A divisão do número {numero1} por dois é: {(numero1/2)}")
 
-divisao_por_dois()
+divide_por_dois()
 
 def calcula_IMC():
     altura = float(input("Digite sua altura:"))
