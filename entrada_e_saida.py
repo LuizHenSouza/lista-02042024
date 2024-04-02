012c565b9c24e7e35946ca34204dc02d591ec7c5
Luiz Souza <LuizCraftPB@Gmail.com>
2024-04-02 20:28:04 -0300

    feat_somar_numeros_add

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -3,3 +3,10 @@ def cumprimentar_usuario():
     print(f"Olá, {nomeUsuario}! Seja bem-vindo(a)!")
 
 cumprimentar_usuario()
+
+def somar_numeros():
+    numero1 = float(input("Digite um número: "))
+    numero2 = float(input("Digite outro número: "))
+    print(f"A soma dos números é: {(numero1+numero2)}")
+
+somar_numeros()
